50f62a3e2ccb09df371cea52f3e70d436ae612a1
@pjts13 <pierre.junior.tsoungui@mni.thm.de>
2026-09-21 20:51:42 +0200

    refactor(document): adjust model and remove unnecessary entity

--- backend/src/main/java/com/pi/backend/model/Application.java
+++ backend/src/main/java/com/pi/backend/model/Application.java
@@ -1,13 +1,11 @@
 package com.pi.backend.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.EnumType;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "applications")
 @Getter
@@ -15,7 +13,6 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 public class Application {
 
     @Id
@@ -23,11 +20,18 @@ public class Application {
     private Long id;
 
     private String company;
-
     private String position;
 
     @Enumerated(EnumType.STRING)
     private ApplicationStatus status;
 
     private LocalDate dateApplied;
+
+    @ManyToMany
+    @JoinTable(
+            name = "application_documents",
+            joinColumns = @JoinColumn(name = "application_id"),
+            inverseJoinColumns = @JoinColumn(name = "document_id")
+    )
+    private List<Document> documents = new ArrayList<>();
 }
--- backend/src/main/java/com/pi/backend/repository/ApplicationDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pi.backend.repository;
-
-import com.pi.backend.model.ApplicationDocument;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationDocumentRepository  extends JpaRepository<ApplicationDocument,Long> {
-}
